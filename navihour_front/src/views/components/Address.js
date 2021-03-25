09d9a3e5d95b7c7fc8f99feb5a650616f6eb68dc
SO KI <sky.apple.radwimps@gmail.com>
2021-03-26 03:25:06 +0900

    sorakun_home_and_address_table

--- navihour_front/src/views/components/Address.js
+++ navihour_front/src/views/components/Address.js
@@ -1,15 +1,118 @@
-import React from 'react';              //Reactを読み込んでいる
-import { Link } from 'react-router-dom';//　追加　Linkタブを読み込む
+// https://material-ui.com/components/tables/
 
-class Address extends React.Component {   //HomeクラスにReact.Componentを継承する
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import StarRateIcon from '@material-ui/icons/StarRate';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import LockOpenIcon from '@material-ui/icons/LockOpen';
+import LockIcon from '@material-ui/icons/Lock';
+import Button from '@material-ui/core/Button';
+import { postApi, getApi } from '../../utils/Api';
 
-    render() {                          //画面表示の為のrenderメソッドを定義する
+// https://material-ui.com/customization/palette/
+
+class Address extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            allAddressList: [],
+        }
+    }
+
+    createData = (addressId, isFavorite, name, address, createdTime, isPrivate) => {
+        return { addressId, isFavorite, name, address, createdTime, isPrivate };
+    }
+
+    // api 完成までのdummyu
+    allAddressList = [
+        this.createData(1, true, '近くのマクドナルド', '池尻大橋', '1994/06/09 18:00:40', true),
+        this.createData(2, true, 'みつおの家', '運河', '2000/06/09 18:00:40', false),
+        this.createData(3, false, '外尾思い出のトイレ', '二子玉川のトイレ', '2015/06/09 18:00:40', false),
+        this.createData(4, false, 'お気に入りの二郎', '神保町', '2017/06/09 18:00:40', true),
+        this.createData(5, true, 'お気に入りのフォーレストラン', 'ベトナム・ホーチミン', '2021/01/09 18:00:40', true),
+    ];
+
+    setAddressList = (addressId, setKey, setValue) => {
+        // const allAddressList = this.state.allAddressList.slice();
+        const allAddressList = this.allAddressList.slice();
+
+        allAddressList.map((addressData) => {
+            if (addressData.addressId === addressId){
+                addressData[setKey] = setValue;
+            }
+        });
+        this.setState({ allAddressList: allAddressList });
+    }
+
+    //API完成後一番最初にたたく想定。
+    //DBから住所をすべて取ってきてstateに入れる。
+    getAllAddress = () => {
+        getApi("Get_All_Address")
+        .then((return_json) => {
+            this.setState({ allAddressList: return_json["all_address_list"] });
+        });
+    }
+
+    changeFavorite = (row) => {
+        // API完成後,コメントアウト解除
+        // const send_json = { addressId: row.addressId, isFavorite: row.isFavorite };
+        // postApi("Favarite_Address", send_json)
+        // .then((return_json) => {
+        //      //resultとmessageが返ってくる
+        // });
+        this.setAddressList(row.addressId, "isFavorite", !row.isFavorite);
+    }
+
+    changePrivate = (row) => {
+        // API完成後,コメントアウト解除
+        // const send_json = { addressId: row.addressId, isPrivate: row.isPrivate };
+        // postApi("Private_Address", send_json)
+        // .then((return_json) => {
+        //      //resultとmessageが返ってくる
+        // });
+        this.setAddressList(row.addressId, "isPrivate", !row.isPrivate);
+    }
+
+    render() {
+        //テスト用
+        // this.setState({allAddressList: this.allAddressList});
+        // const allAddressList = this.state.allAddressList;
+        const allAddressList = this.allAddressList;
         return (
-            <div>
-                Address<br/>
-            </div>
+            <TableContainer component={Paper}>
+                <Table aria-label="customized table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Favorite☆</TableCell>
+                            <TableCell align="right">Name</TableCell>
+                            <TableCell align="right">Address</TableCell>
+                            <TableCell align="right">Created Time</TableCell>
+                            <TableCell align="right">Privete</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {allAddressList.map((row) => (
+                            <TableRow>
+                                <TableCell><Button onClick={() => { this.changeFavorite(row) }}>{row.isFavorite ? <StarRateIcon style={{ color: "#004d40" }} /> : <StarBorderIcon />}</Button></TableCell>
+                                <TableCell align="right">{row.name}</TableCell>
+                                <TableCell align="right">{row.address}</TableCell>
+                                <TableCell align="right">{row.createdTime}</TableCell>
+                                <TableCell><Button onClick={() => { this.changePrivate(row) }}>{row.isPrivate ? <LockIcon /> : <LockOpenIcon />}</Button></TableCell>
+                            </TableRow>
+                        ))
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         );
     }
 }
 
-export default Address;                   //Homeを出力する為に必要
+export default Address;
--- navihour_front/src/views/components/Home.js
+++ navihour_front/src/views/components/Home.js
@@ -1,18 +1,17 @@
-import React from 'react';              //Reactを読み込んでいる
-import { Link, Router, Route } from 'react-router-dom';//　追加　Linkタブを読み込む
+import React from 'react';
+import Header from '../../utils/Header';
+import Test2 from './Test2'
 import Address from './Address'
-import Profile from './Profile'
 
-class Home extends React.Component {   //HomeクラスにReact.Componentを継承する
-
-    render() {                          //画面表示の為のrenderメソッドを定義する
-        return (
-            <div>
-                Home<br/>
-                <Link to={`/Test2`}>Go To Test2</Link>
-            </div>
-        );
+class Home extends React.Component {
+    render() {
+            return (
+                <div>
+                    <Header />
+                    <Address />
+                </div>
+            );
+        }
     }
-}
 
-export default Home;                   //Homeを出力する為に必要
+export default Home;
