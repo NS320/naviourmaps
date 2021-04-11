6d335e07d92f7cd6c809686b117b7ecfa484730c
NS320 <tuscycling320@gmail.com>
2021-04-11 16:43:46 +0900

    mitsuo_setting.py微修正

--- nmproject/nmproject/settings.py
+++ nmproject/nmproject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'mapapp.apps.MapappConfig',
     'myappA',
     'rest_framework',
+    'corsheaders',
 ]
 
 #rest_frameworkの設定
@@ -52,6 +53,7 @@ REST_FRAMEWORK = {
 '''
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -59,6 +61,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
 ]
 
 ROOT_URLCONF = 'nmproject.urls'
