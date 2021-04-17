160c557af364c6d1bb84bf85fb9bbe4903f44e38
NS320 <tuscycling320@gmail.com>
2021-04-18 00:16:49 +0900

    mitsuo_HomeとMapのstateのやりとりヘルプ

--- navihour_front/src/views/components/maps/Map.js
+++ navihour_front/src/views/components/maps/Map.js
@@ -1,4 +1,4 @@
-import React, { Component, createRef } from 'react'
+import React, { Component, createRef } from 'react';
 import googleApiJson from '../../../googleAPI.json';
 import CreateRoute from './map_functions/CreateRoute'
 
@@ -6,12 +6,22 @@ class Map extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            route_exist:false,//ルート存在フラグ
+            start_address: {          
+                address: this.props.StartAddress["address"],  
+                lat: this.props.StartAddress["lat"],
+                lng: this.props.StartAddress["lng"]
+            },
+            goal_address: {   
+                address: this.props.GoalAddress["address"],           
+                lat: this.props.GoalAddress["lat"],
+                lng: this.props.GoalAddress["lng"]
+            },
+            route_exist: false,//ルート存在フラグ
         }
         this.map = {};
         this.directionsRenderer = {};
     }
-    
+
     googleMapRef = React.createRef()
 
     componentDidMount() {
@@ -20,9 +30,9 @@ class Map extends Component {
         var json = googleApiJson;
         var key = json["googleAPI"];
         googleMapScript.src = "https://maps.googleapis.com/maps/api/js?key=" + key + " &libraries=places"
-        
-        window.document.body.appendChild(googleMapScript)
 
+        window.document.body.appendChild(googleMapScript)
+        
         googleMapScript.addEventListener('load', () => {
             this.googleMap = this.createGoogleMap()
             this.marker = this.createMarker()
@@ -30,22 +40,24 @@ class Map extends Component {
     }
 
     componentDidUpdate() {
+        console.log(this.state.start_address)
+        console.log(this.props.StartAddress)
         //StartAddressのaddressがnull場合ルートを消す処理
-        if((!this.props.StartAddress["address"]) && this.state.route_exist){
+        if ((!this.state.start_address["address"]) && this.state.route_exist) {
             this.directionsRenderer.setMap(null);
             this.directionsRenderer = {};
             this.setRouteExist(false);
         }
         //StartAddressとGoalAddressのaddressが存在する場合ルートを引く処理
-        if(this.props.StartAddress["address"] && this.props.GoalAddress["address"] && (!this.state.route_exist)){
-            //CreateRoute(this.directionsRenderer, this.map, this.props.StartAddress, this.props.GoalAddress);
+        if (this.state.start_address["address"] && this.state.goal_address["address"] && (!this.state.route_exist)) {
+            //CreateRoute(this.directionsRenderer, this.map, this.props.StartAddress, this.props.GoalAddress);//残骸
             //console.log(this.directionsRenderer);
             this.createRoute();
         }
     }
 
     setRouteExist = (route_exist) => {
-        this.setState({route_exist: route_exist});
+        this.setState({ route_exist: route_exist });
     }
 
     createGoogleMap = () => {
@@ -58,7 +70,7 @@ class Map extends Component {
             streetViewControl: false,
         })
 
-        this.map.addListener('click', function(e){
+        this.map.addListener('click', function (e) {
             //map.panTo(e.latLng); // クリックした場所をマップの中心にする。
             this.getAddress(e.latLng);
         }.bind(this));
@@ -73,51 +85,51 @@ class Map extends Component {
 
     getAddress = (latlng) => {
         var geocoder = new window.google.maps.Geocoder();
- 
+
         geocoder.geocode({
-          latLng: latlng
-        }, function(results, status) {
-          if (status === window.google.maps.GeocoderStatus.OK) {
-            if (results[0].geometry) {
-              var address = results[0].formatted_address.replace(/^日本(、|,)/, '');
-              this.setAddress(address, latlng);
+            latLng: latlng
+        }, function (results, status) {
+            if (status === window.google.maps.GeocoderStatus.OK) {
+                if (results[0].geometry) {
+                    var address = results[0].formatted_address.replace(/^日本(、|,)/, '');
+                    this.setAddress(address, latlng);
+                }
+            } else {
+                // Todo 取得失敗時の仕様検討
+                alert("住所取得に失敗:" + results);
             }
-          } else {
-            // Todo 取得失敗時の仕様検討
-            alert("住所取得に失敗:" + results);
-          }
-          
+
         }.bind(this));
     }
 
     setAddress = (address, latlng) => {
-        if(!this.props.StartAddress["address"]){
-            this.props.setStartAddress({address: address, lat: latlng.lat(), lng: latlng.lng()});
+        if (!this.state.start_address["address"]) {
+            this.props.setStartAddress({ address: address, lat: latlng.lat(), lng: latlng.lng() });
         }
-        else if(!this.props.GoalAddress["address"]){
-            this.props.setGoalAddress({address: address, lat: latlng.lat(), lng: latlng.lng()});
+        else if (!this.state.goal_address["address"]) {
+            this.props.setGoalAddress({ address: address, lat: latlng.lat(), lng: latlng.lng() });
         }
-        else{
+        else {
             // Todo 仮置きのメッセージ
             alert("3点以上の設定はできません。");
         }
     }
 
     createRoute = () => {
-        var startLatLng = new window.google.maps.LatLng(this.props.StartAddress["lat"], this.props.StartAddress["lng"]);
-        var goalLatLng = new window.google.maps.LatLng(this.props.GoalAddress["lat"], this.props.GoalAddress["lng"]);
+        var startLatLng = new window.google.maps.LatLng(this.state.start_address["lat"], this.state.start_address["lng"]);
+        var goalLatLng = new window.google.maps.LatLng(this.state.goal_address["lat"], this.state.goal_address["lng"]);
         var directionsService = new window.google.maps.DirectionsService();
         this.directionsRenderer = new window.google.maps.DirectionsRenderer();
-    
+
         var request = {
             origin: startLatLng, //スタート地点
             destination: goalLatLng, //ゴール地点
             travelMode: window.google.maps.DirectionsTravelMode.BICYCLING, //移動手段
         };
-        directionsService.route(request, function(result, status) {
+        directionsService.route(request, function (result, status) {
             if (status == window.google.maps.DirectionsStatus.OK) {
                 this.directionsRenderer.setOptions({
-                    preserveViewport: true //ズーム率を変更してルート全体を表示しない
+                    preserveViewport: false //ズーム率を変更してルート全体を表示する
                 });
                 // ルート検索の結果を地図上に描画
                 this.directionsRenderer.setDirections(result);
@@ -138,13 +150,13 @@ class Map extends Component {
                     style={{ width: '800px', height: '500px' }}
                 />
                 <div className="map_state">
-                    選択1：{this.props.StartAddress["address"]} <br/>
-                    緯度：{this.props.StartAddress["lat"]} <br/>
-                    経度：{this.props.StartAddress["lng"]} <br/>
-                    <br/>
-                    選択2：{this.props.GoalAddress["address"]} <br/>
-                    緯度：{this.props.GoalAddress["lat"]} <br/>
-                    経度：{this.props.GoalAddress["lng"]} <br/>
+                    選択1：{this.state.start_address["address"]} <br />
+                    緯度：{this.state.start_address["lat"]} <br />
+                    経度：{this.state.start_address["lng"]} <br />
+                    <br />
+                    選択2：{this.state.goal_address["address"]} <br />
+                    緯度：{this.state.goal_address["lat"]} <br />
+                    経度：{this.state.goal_address["lng"]} <br />
                 </div>
             </div>
         )
